cf4824381e08adbe80bf01f8eab2152faef5ce4a
Kaykross <constantaddo@gmail.com>
2023-09-26 08:07:23 +0000

    second commit

--- entities.js
+++ entities.js
@@ -3,11 +3,25 @@ const items = document.querySelector(".content .items");
 const content = document.querySelector(".content");
 const sidebar = document.querySelector(".sidebar");
 const sideLinks = document.querySelector(".side-links .items");
+const menuIcon = document.querySelector(".open-sidebar");
+const date = document.querySelector(".footer .date");
 
+loadHTML();
+addDate(date);
+search.oninput=e=>loadHTML(search.value);
+menuIcon.onclick=e=>sidebar.classList.toggle('active');
+
+
+//generate range of numbers from min to max [1,2,3];
+function range(min=0,max=1000){
+    const numbers = [];
+    for (let i = min; i <= max; i++) { numbers.push(i)}
+    return numbers;
+}
+
+//map the range of numbers array into a html and add to the page
 function loadHTML(val){
-    
-    console.log(val);
-    const arr = range(0,1000).filter((i,n,arr)=>i==val?i:val == undefined ?arr: val == "" ?arr:null);
+    const arr = range(33,6000).filter((i,n,arr)=>i==val?i:val == undefined ?arr: val == "" ?arr:null);
     if(arr.length == 0){
         sideLinks.innerHTML = '';
         items.innerHTML = `<div class="item">Entity Not Found</div>`;
@@ -16,8 +30,8 @@ function loadHTML(val){
     }
     const iu = arr.map(e=>`
     <div class="item" id="${e}">
-        <div>&#${e};</div>
-        <div class="name"></div>
+        <div class="entity">&#${e};</div>
+        <div class="label"></div>
     </div>`
     ).join("");
     items.innerHTML = iu;
@@ -26,34 +40,14 @@ function loadHTML(val){
     [...sideLinks.children].forEach(i=>i.children[1].textContent = `&#${i.id};`);
     content.append(items);
 }
-loadHTML();
 
-search.oninput=e=>{
-    console.log(search.value == "");
-    loadHTML(search.value)
+function addDate(el){
+   const d = new Date().toLocaleDateString("en-gh",{dateStyle:"long"});
+   return el.innerText = d;
 }
 
 
 
 
 
-function range(min=0,max=1000){
-    const numbers = [];
-    for (let i = min; i <= max; i++) {
-        numbers.push(i)
-    }
-    return numbers;
-}
 
-function render(htmlString){ 
-   const node = document.createRange().createContextualFragment(`<div>${htmlString}</div>`);
-   const nodes = node.firstElementChild.children;
-   return node;
-};
-
-const btnOpenSidebar = document.querySelector(".open-sidebar");
-btnOpenSidebar.onclick=e=>{
-    console.log("fired");
-    sidebar.classList.toggle('active');
-    content.classList.toggle('active');
-}
